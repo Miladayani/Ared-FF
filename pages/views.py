ca9e176863daf5bb8127301b3598840e61ba8274
Your Name <you@example.com>
2024-12-23 13:37:51 +0330

    add Homepage view ad other pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class RestaurantShop(TemplateView):
+    template_name = 'home.html'
+
+
+class HomeFastFood(TemplateView):
+    template_name = 'home2.html'
+
+
+class HomeRestaurant(TemplateView):
+    template_name = 'home3.html'
+
+
+class AboutUs(TemplateView):
+    template_name = 'pages/aboutus.html'
+
+
+class Service(TemplateView):
+    template_name = 'pages/service.html'
+
+
+class ServiceDetails(TemplateView):
+    template_name = 'pages/servicedetails.html'
+
+
+class Chef(TemplateView):
+    template_name = 'pages/chef.html'
+
+
+class ChefDetails(TemplateView):
+    template_name = 'pages/chefdetails.html'
+
+
+class Reservation(TemplateView):
+    template_name = 'pages/reservation.html'
+
+
+class Gallery(TemplateView):
+    template_name = 'pages/gallery.html'
+
+
+class ErrorPage(TemplateView):
+    template_name = 'pages/error.html'
