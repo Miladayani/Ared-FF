c90609e206ea3a4faa1fa8d9da6c86b56e0b7245
Your Name <you@example.com>
2024-12-29 11:57:51 +0330

    create food model

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Food(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    # image =
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
