b6055f423ecb6a419536221afddfd7ff7d440965
Your Name <you@example.com>
2025-01-25 12:11:09 +0330

    add tabular inline for comments

--- foods/admin.py
+++ foods/admin.py
@@ -3,21 +3,35 @@ from django.contrib import admin
 from .models import Pizza, Sandwich, Comment
 
 
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ('body', 'author', 'active', 'rating', )
+    extra = 1
+
+
 @admin.register(Pizza)
 class PizzaAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'active', 'size', 'image', )
+    list_display = ['title', 'price', 'active', 'size', 'image', ]
+
+    inlines = [
+        CommentsInline,
+    ]
 
 
 @admin.register(Sandwich)
 class SandwichAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'active', 'image', )
+    list_display = ['title', 'price', 'active', 'image', ]
+
+    inlines = [
+        CommentsInline,
+    ]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('get_product', 'rating', 'author', 'body', 'email', 'date_created', 'active', )
-    list_filter = ('active', 'rating', 'date_created')
-    search_fields = ('body', 'email', 'author__username', )
+    list_display = ['get_product', 'rating', 'author', 'body', 'email', 'date_created', 'active', ]
+    list_filter = ['active', 'rating', 'date_created']
+    search_fields = ['body', 'email', 'author__username', ]
 
     def get_product(self, obj):
         if obj.pizza:
