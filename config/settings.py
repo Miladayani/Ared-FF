062a77b3a13f937dae8ac60752b38cd011b4b218
Your Name <you@example.com>
2025-02-05 10:57:57 +0330

    add jalali date support and humanize integers with commas

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     # third party libraries
+    'jalali_date',
     'crispy_forms',
     'crispy_bootstrap5',
     'allauth',
