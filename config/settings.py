a0ce2fc3633355d5c1a16eb2182cd1f69f79db34
Your Name <you@example.com>
2025-02-25 14:53:24 +0330

    move sensitive emial credentials to enviroment variables

--- config/settings.py
+++ config/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'miladayani526@gmail.com'  # your email
-EMAIL_HOST_PASSWORD = 'stgkwfsvvizoiclq'  # App Password
+EMAIL_HOST_USER = env.str('DJANGO_EMAIL_HOST_USER')  # your email
+EMAIL_HOST_PASSWORD = env.str('DJANGO_EMAIL_HOST_PASSWORD')  # App Password
 
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
